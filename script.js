32d3dd81f5cf323380edbc5a64372f0b50f01298
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-06-20 17:51:02 +0200

    Continuer jusqu'a etape 5

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
-function demandeNombre(){
-    let givenNumber = Number(prompt("Donner un numéro : "))
-    return givenNumber
+const chiffreHTML = document.getElementById('chiffre_html')
+const indice = document.getElementById('indice')
+const chiffreADeviner = document.getElementById('chiffre_a_deviner')
+const guess = document.getElementById('guess')
+const bouttonValiderGuess = document.getElementById('btn_valider_guess')
+const bouttonValiderChiffreADeviner = document.getElementById('btn_chiffre_a_deviner')
+const labelChiffreADeviner = document.getElementById('label_chiffre_a_deviner')
+const labelGuess = document.getElementById('bouttonValiderGuess')
+const tentative = document.getElementById('tentative')
+count=0
+nombreMystere=0
+
+/*
+Fonction playJoueurUn() au joueur 1 d'écrire un nombre entre 0 et 50 (chiffre à deviner par joueur 2)
+*/
+function playJoueurUn(){
+    let nombreMystere = Number(chiffreADeviner.value)
+    if (nombreMystere > 50) {
+        alert(`C'est un nombre entre 0 et 50 qu'on demande Joueur1`)
+        return
+    }
+    labelChiffreADeviner.hidden = true
+    bouttonValiderChiffreADeviner.hidden = true
+    chiffreADeviner.hidden = true
+    console.log(nombreMystere)
+    return nombreMystere
 }
 
-function didIWin(givenNumber){
-    if (givenNumber < 22){
-        alert('Plus grand !')
-    } else if (givenNumber > 22){
-        alert('Plus petit !')
+bouttonValiderChiffreADeviner.addEventListener('click',playJoueurUn)
+
+/*
+Fonction didIWin() permet d'afficher un indice au joueur 2 si le nombre donné est trop grand ou trop petit 
+par rapport au nombre à deviner. 2 paramètres :
+    - givenNumber : Number
+    - nombreTrouve : Number
+*/
+function didIWin(nombreMystere){
+    let givenNumber = guess.value
+    console.log(givenNumber)
+    if (givenNumber < nombreMystere){
+        indice.innerText = 'Indice : Plus grand !'
+        count ++   
+    } else if (givenNumber > nombreMystere){
+        indice.innerText = 'Indice : Plus petit !'
+        count++
     } else {
-        alert('Bravo! Vous avez deviné le nombre !')
+        chiffreHTML.innerText = 'Bravo! Vous avez deviné le nombre !'
+        guess.hidden = true
+        bouttonValiderGuess.hidden = true
+        labelGuess.hidden = true
+        indice.hidden = true
+    }
+    tentative.innerText = `Nombre de tentative = ${count}`
+}
+
+bouttonValiderGuess.addEventListener('click', didIWin)
+
+/*
+Fonction gameplay() permet d'appeler les fonctions :
+    - playJoueurUn()
+    - demandeNombre()
+    - didIWin()
+*/
+function gamePlay(){
+    let nombreMystere = playJoueurUn()
+    let givenNumber = demandeNombre()
+    let count = 1
+    while (didIWin(givenNumber, nombreMystere) == false) {
+        givenNumber = demandeNombre()
+        count ++
+
+
     }
 }
-
-function gamePlay(){
-    
-}
-
-let nombre = demandeNombre()
-didIWin(nombre)
