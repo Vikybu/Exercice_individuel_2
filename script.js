22552c01681b027ed728d7a64b39402cd09bf545
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-06-20 19:25:31 +0200

    Fin etape 5 et 6

--- script.js
+++ script.js
@@ -7,26 +7,28 @@ const bouttonValiderChiffreADeviner = document.getElementById('btn_chiffre_a_dev
 const labelChiffreADeviner = document.getElementById('label_chiffre_a_deviner')
 const labelGuess = document.getElementById('bouttonValiderGuess')
 const tentative = document.getElementById('tentative')
-count=0
-nombreMystere=0
+let count=0
+let nombreMystere = 0
+let bornMin = 0
+let bornMax = 50
 
 /*
 Fonction playJoueurUn() au joueur 1 d'écrire un nombre entre 0 et 50 (chiffre à deviner par joueur 2)
 */
 function playJoueurUn(){
-    let nombreMystere = Number(chiffreADeviner.value)
-    if (nombreMystere > 50) {
-        alert(`C'est un nombre entre 0 et 50 qu'on demande Joueur1`)
+    nombreMystere = Number(chiffreADeviner.value)
+    if (nombreMystere > bornMax || nombreMystere < bornMin) {
+        alert(`C'est un nombre entre ${bornMin} et ${bornMax} qu'on demande Joueur1`)
         return
     }
     labelChiffreADeviner.hidden = true
     bouttonValiderChiffreADeviner.hidden = true
     chiffreADeviner.hidden = true
     console.log(nombreMystere)
-    return nombreMystere
 }
 
 bouttonValiderChiffreADeviner.addEventListener('click',playJoueurUn)
+
 
 /*
 Fonction didIWin() permet d'afficher un indice au joueur 2 si le nombre donné est trop grand ou trop petit 
@@ -34,23 +36,32 @@ par rapport au nombre à deviner. 2 paramètres :
     - givenNumber : Number
     - nombreTrouve : Number
 */
-function didIWin(nombreMystere){
-    let givenNumber = guess.value
+function didIWin(){
+    let givenNumber = Number(guess.value)
     console.log(givenNumber)
+
     if (givenNumber < nombreMystere){
         indice.innerText = 'Indice : Plus grand !'
-        count ++   
+        count ++
+        if (givenNumber > bornMin) {
+            bornMin = givenNumber 
+        }
+        chiffreHTML.innerText = `Le chiffre à trouver se trouve entre : ${bornMin} < ? < ${bornMax}`
     } else if (givenNumber > nombreMystere){
         indice.innerText = 'Indice : Plus petit !'
         count++
-    } else {
+        if (givenNumber < bornMax) {
+            bornMax = givenNumber 
+        }
+        chiffreHTML.innerText = `Le chiffre à trouver se trouve entre : ${bornMin} < ? < ${bornMax}`
+    } else if (givenNumber == nombreMystere) {
         chiffreHTML.innerText = 'Bravo! Vous avez deviné le nombre !'
         guess.hidden = true
         bouttonValiderGuess.hidden = true
         labelGuess.hidden = true
         indice.hidden = true
     }
-    tentative.innerText = `Nombre de tentative = ${count}`
+    tentative.innerText = `Nombre de tentative = ${count+1}`
 }
 
 bouttonValiderGuess.addEventListener('click', didIWin)
